42d8112b8e8e89d72a665567ffa13c5d83697b0b
liuguicen <2583657917@qq.com>
2016-05-23 02:54:41 +0800

    2016.5.22-liuguicen

--- app/src/main/java/a/baozouptu/control/AddTextFragment.java
+++ app/src/main/java/a/baozouptu/control/AddTextFragment.java
@@ -28,6 +28,7 @@ public class AddTextFragment extends Fragment {
     LinearLayout typeface;
     LinearLayout special;
     LinearLayout bouble;
+
     @Override
     public void onAttach(Context context) {
 
@@ -44,37 +45,60 @@ public class AddTextFragment extends Fragment {
         setClick();
         return view;
     }
+
     private void initView(View view) {
-        toumingdu=(LinearLayout)view.findViewById(R.id.add_text_toumingdu);
-        style=(LinearLayout)view.findViewById(R.id.add_text_style);
-        color=(LinearLayout) view.findViewById(R.id.add_text_color);
-        typeface=(LinearLayout)view.findViewById(R.id.add_text_typeface);
-        special=(LinearLayout)view.findViewById(R.id.add_text_special);
-        bouble=(LinearLayout)view.findViewById(R.id.add_text_bubble);
+        toumingdu = (LinearLayout) view.findViewById(R.id.add_text_toumingdu);
+        style = (LinearLayout) view.findViewById(R.id.add_text_style);
+        color = (LinearLayout) view.findViewById(R.id.add_text_color);
+        typeface = (LinearLayout) view.findViewById(R.id.add_text_typeface);
+        special = (LinearLayout) view.findViewById(R.id.add_text_special);
+        bouble = (LinearLayout) view.findViewById(R.id.add_text_bubble);
     }
+
     private void setClick() {
         toumingdu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Dialog tmdDialog=new Dialog(mcontext);
+                Dialog tmdDialog = new Dialog(mcontext);
                 tmdDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
                 tmdDialog.setContentView(R.layout.dialog_layout_toumindu);
 
-                Window dialogWindow = tmdDialog.getWindow();
-                dialogWindow.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM);
-                dialogWindow.setBackgroundDrawableResource(R.drawable.dialog_background);
-
-                WindowManager.LayoutParams lp = dialogWindow.getAttributes();
-                lp.flags= 0;
-                lp.y=v.getHeight()+Util.dp2Px(mcontext,20);
-                dialogWindow.setAttributes(lp);
-                SeekBar tmdSeekbar=(SeekBar)tmdDialog.findViewById(R.id.seekbar_toumingdu);
+                setDialogLayout(tmdDialog,v.getHeight());
+                SeekBar tmdSeekbar = (SeekBar) tmdDialog.findViewById(R.id.seekbar_toumingdu);
                 tmdDialog.show();
+            }
+        });
+        color.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog colorDialog = new Dialog(mcontext);
+                colorDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                colorDialog.setContentView(R.layout.dialog_layout_color);
+
+
+                setDialogLayout(colorDialog,v.getHeight());
+
+                SeekBar tmdSeekbar = (SeekBar) colorDialog.findViewById(R.id.seekbar_toumingdu);
+                colorDialog.show();
             }
         });
     }
 
+    /**
+     *
+     * @param dialog
+     * @param height 相对高度
+     */
+    void setDialogLayout(Dialog dialog,int height) {
+        Window dialogWindow = dialog.getWindow();
+        dialogWindow.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM);
+        //dialogWindow.setBackgroundDrawableResource(R.drawable.dialog_background);
 
+        WindowManager.LayoutParams lp = dialogWindow.getAttributes();
+        lp.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL;
+        lp.y = height+ Util.dp2Px(mcontext, 20);
+        dialogWindow.setAttributes(lp);
+    }
 
     @Override
     public void onDestroy() {
---
+++ test/src/main/java/com/example/administrator/test/TestClass.java
@@ -0,0 +1,8 @@
+package com.example.administrator.test;
+
+/**
+ * Created by Administrator on 2016/5/22.
+ */
+public class TestClass {
+}
+
