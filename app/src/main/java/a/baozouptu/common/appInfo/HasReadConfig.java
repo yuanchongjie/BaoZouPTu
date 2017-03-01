38277fa73d1672c8390a0fc3a8bfafc54640c9c3
liuguicen <2583657917@qq.com>
2017-03-02 03:21:55 +0800

    第三版，一半的功能增加

---
+++ app/src/main/java/a/baozouptu/common/appInfo/HasReadConfig.java
@@ -0,0 +1,37 @@
+package a.baozouptu.common.appInfo;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import a.baozouptu.common.dataAndLogic.AllData;
+
+public class HasReadConfig {
+
+    private final SharedPreferences sp;
+
+    public HasReadConfig() {
+        sp = AllData.appContext.getSharedPreferences("common_config", Context.MODE_PRIVATE);
+    }
+
+    /**
+     * 常用图片使用阅读与否
+     */
+    public boolean hasReadUsuPicUse() {
+        return sp.getBoolean("usu_pic_use", false);
+    }
+
+    public void write_usuPicUse(boolean isRead) {
+        sp.edit().putBoolean("usu_pic_use", isRead)
+                .apply();
+    }
+
+    public boolean hasReadGoSend() {
+        return sp.getBoolean("go_send", false);
+    }
+
+    public void write_GoSend(boolean isRead) {
+        sp.edit().putBoolean("go_send", isRead)
+                .apply();
+    }
+
+}
